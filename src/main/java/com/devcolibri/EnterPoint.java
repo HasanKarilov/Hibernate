866c797928ee7074def932ab3d5eb7b3c9a613cc
HasanKarilov <hkarilov@gmail.com>
2017-05-25 22:10:53 +0600

    Hibernate some operation

--- src/main/java/com/devcolibri/EnterPoint.java
+++ src/main/java/com/devcolibri/EnterPoint.java
@@ -5,37 +5,63 @@ import com.devcolibri.entity.Developer;
 import com.devcolibri.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class EnterPoint
 {
-    private static SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
     public static void main(String[] args) {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
-        System.out.println("Maven + Hibernate + MySQL");
-        Session session = sessionFactory.openSession();
 
+        System.out.println("Maven + Hibernate + MySQL");
+
+        EnterPoint enterPoint = new EnterPoint();
+
+        enterPoint.addDev("Adilet", "Ruby Developer", 35000);
+        enterPoint.addDev("Ulan", "Php Developer", 30000);
+        enterPoint.updateDev(3, 40000);
+
+
+        for(Developer developer: enterPoint.listDevs())
+        {
+            System.out.println(developer.toString());
+        }
+    }
+
+    private List<Developer> listDevs(){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<Developer> devs = session.createQuery("from Developer").list();
+
+        transaction.commit();
+        session.close();
+        return devs;
+    }
+
+    private void addDev(String name, String specialty, int salary){
+        Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        User user = new User();
-
-        user.setFirstName("Askat");
-        user.setLastName("Karilov");
-
-        session.save(user);
-
-        Developer dev = new Developer();
-
-        dev.setName("Han");
-        dev.setSpecialty("Java Developer");
-        dev.setSalary(45000);
-
+        Developer dev = new Developer(name, specialty, salary);
         session.save(dev);
         session.getTransaction().commit();
-
         session.close();
     }
 
+    private void updateDev(int iD, int newSalary){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Developer dev = (Developer) session.get(Developer.class, iD);
+        dev.setSalary(newSalary);
+        session.update(dev);
+
+        transaction.commit();
+        session.close();
+    }
 
 }
